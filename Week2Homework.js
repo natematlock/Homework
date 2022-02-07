df5e2907380251e757bb656be75f3447cf2eb0df
Nathaniel Matlock <natematlock2@gmail.com>
2022-02-07 16:11:46 -0600

    first commit, Homework

---
+++ Week2Homework.js
@@ -0,0 +1,13 @@
+const pizzaPlace = "Hungry Phil's";
+let numberOfToppings = 10;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(`"come on down to ${pizzaPlace}, where the pizza is guaranteed"`);
+
+if (numberOfToppings >= 10) {
+    console.log("A whole lot of pizza.")
+} else {
+    console.log("Quality, not quantity.")
+}
